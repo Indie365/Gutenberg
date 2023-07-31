331c666e26e1e5629e975bf5ceeb9fa140ac2dcf
Glen Davies <glen.davies@automattic.com>
2023-07-31 18:12:30 +1200

    More e2e test fixes

--- packages/e2e-tests/specs/editor/various/reusable-blocks.test.js
+++ packages/e2e-tests/specs/editor/various/reusable-blocks.test.js
@@ -195,7 +195,7 @@ describe( 'Reusable blocks', () => {
 
 		// Convert block to a reusable block.
 		await clickBlockToolbarButton( 'Options' );
-		await clickMenuItem( 'Create pattern/reusable block' );
+		await clickMenuItem( 'Create pattern' );
 
 		// Set title.
 		const nameInput = await page.waitForSelector(
@@ -379,7 +379,7 @@ describe( 'Reusable blocks', () => {
 
 		// Convert to reusable.
 		await clickBlockToolbarButton( 'Options' );
-		await clickMenuItem( 'Create pattern/reusable block' );
+		await clickMenuItem( 'Create pattern' );
 		const nameInput = await page.waitForSelector(
 			reusableBlockNameInputSelector
 		);
